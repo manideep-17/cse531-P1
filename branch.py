655e677d5e3ce55dcdbfdb52203bf67b8b7c3bfa
Manideep Reddy Aliminati <manideepreddyaliminati@manideepslaptop.dhcp.asu.edu>
2023-09-28 16:42:56 -0700

    Processes created and stored in dict (branch & customers).

--- branch.py
+++ branch.py
@@ -1,9 +1,13 @@
+from concurrent import futures
+import sys
+import json
+
 import grpc
-import example_pb2
-import example_pb2_grpc
+import bank_pb2
+import bank_pb2_grpc
 
 
-class Branch(example_pb2_grpc.RPCServicer):
+class Branch(bank_pb2_grpc.BankServicer):
     def __init__(self, id, balance, branches):
         # unique ID of the Branch
         self.id = id
@@ -22,3 +26,41 @@ class Branch(example_pb2_grpc.RPCServicer):
     # TODO: students are expected to process requests from both Client and Branch
     def MsgDelivery(self, request, context):
         pass
+
+
+def start_grpc_servers(branches):
+    servers = []
+
+    for i in range(len(branches)):
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        branch = Branch(id=branches[i]["id"],
+                        balance=branches[i]["balance"], branches=[])
+        bank_pb2_grpc.add_BankServicer_to_server(branch, server)
+        port = 50051 + branches[i]["id"]
+        server.add_insecure_port(f'[::]:{port}')
+        server.start()
+        print(f"Branch {branches[i]['id']} started on port: {port}")
+        servers.append(server)
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        print("\nStopping all servers.")
+        for server in servers:
+            server.stop(0)
+
+
+if __name__ == '__main__':
+    # Fetch the json path from the argv and read the JSON input
+    file_path = f'{sys.argv[1]}'
+    with open(file_path, 'r') as json_file:
+        data = json.load(json_file)
+
+    branches = []
+
+    for i in range(len(data)):
+        if (data[i]["type"] == "branch"):
+            branches.append(data[i])
+
+    start_grpc_servers(branches)
--- customer.py
+++ customer.py
@@ -1,7 +1,10 @@
-import grpc
-import example_pb2
-import example_pb2_grpc
 import time
+import sys
+import json
+
+import grpc
+import bank_pb2
+import bank_pb2_grpc
 
 
 class Customer:
@@ -14,11 +17,45 @@ class Customer:
         self.recvMsg = list()
         # pointer for the stub
         self.stub = None
+        self.port = 50051 + id
+        self.lastProcessedId = 0
+
+    def appendEvents(self, events):
+        self.events.append(events)
 
     # TODO: students are expected to create the Customer stub
     def createStub(self):
-        pass
+        with grpc.insecure_channel(f"localhost:{self.port}") as channel:
+            stub = bank_pb2_grpc.BankStub(channel)
+        return stub
 
     # TODO: students are expected to send out the events to the Bank
     def executeEvents(self):
+        if self.stub is None:
+            self.stub = self.createStub()
+        result = []
+        for i in range(len(self.events)):
+            print("processing event")
         pass
+
+
+if __name__ == '__main__':
+    # Fetch the json path from the argv and read the JSON input
+    file_path = f'{sys.argv[1]}'
+    with open(file_path, 'r') as json_file:
+        data = json.load(json_file)
+
+    customerData = []
+    customers = {}
+    response = []
+
+    for i in range(len(data)):
+        if (data[i]["type"] == "customer"):
+            customerData.append(data[i])
+        if data[i]["id"] not in customers:
+            customers[data[i]["id"]] = Customer(
+                data[i]["id"], data[i]["events"])
+            response.append(customers[data[i]["id"]].executeEvents())
+        else:
+            customers[data[i]["id"]].appendEvents(data[i]["events"])
+            response.append(customers[data[i]["id"]].executeEvents())
